d84e577a44bd6b5af2b61f98c3a5733cb0ab9d71
waynero20 <waynerondina20@gmail.com>
2024-10-05 13:06:47 +0800

    rest updates

--- rest/src/routers/transaction.router.ts
+++ rest/src/routers/transaction.router.ts
@@ -3,12 +3,14 @@ import { createTransaction } from '../services/transaction.service';
 
 const transactionRouter = express.Router();
 
-transactionRouter.post('/', async(req: Request, res: Response) => {
+transactionRouter.post('/', async (req: Request, res: Response) => {
   try {
+    console.log('here')
+
     const userId = String(req.user?.id);
-    const {salesAgreementId} = req.body;
-    
-    const created = await createTransaction({leadId: userId, salesAgreementId});
+    const { salesAgreementId } = req.body;
+
+    const created = await createTransaction({ leadId: userId, salesAgreementId });
     if (!created) {
       throw new Error('Failed to create transaction');
     }
@@ -16,7 +18,8 @@ transactionRouter.post('/', async(req: Request, res: Response) => {
     return res.status(200).json(created);
 
   } catch (error) {
-    return res.status(500).json({message: 'Internal server error'})
+    console.log('error', error)
+    return res.status(500).json({ message: 'Internal server error' })
   }
 })
 export default transactionRouter;
